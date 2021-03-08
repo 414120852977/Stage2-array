ef753918a28a4a4b01846e5af772a46a547917cd
Ashok Mane <ashokmane81800@gmail.com>
2021-03-08 17:17:23 +0530

    [ashok] created array.sh

---
+++ array.sh
@@ -0,0 +1,35 @@
+#! /bin/bash -x
+#declaring array
+#declare -a cities
+#cities=(mumbai pune nashik nagpur)
+#printing array
+#echo ${cities[@]}
+#echo ${cities[1]} ${cities[2]}
+#iterating values
+#for city in ${cities[@]}
+#do
+#	echo "city"  $city
+#done
+#change name of values
+#cities[2]="thane"
+#cities[0]="navimumbai"
+
+#echo "length" ${#cities[@]}
+#echo "index" ${!cities[@]}
+#for index in ${!cities[@]}
+#do
+#	echo "index is" $index
+
+#done
+#for inex-var in ${!cities[@]}
+#do
+#	echo "element " ${!cities[@]}
+#done
+#read -p "enter the number" n
+#i=0
+#while [ $i -lt $n]
+#do
+#	read cities[$i]
+#	i=$((i+1))
+#done
+
